922ba0c823f6eca1f316424f26d0a5e4706b17b1
kregoric <isaac.a.kregor@gmail.com>
2024-03-21 17:20:46 -0400

    Init

--- src/ExtraCreditMethods.java
+++ src/ExtraCreditMethods.java
@@ -1,2 +1,56 @@
-package PACKAGE_NAME;public class ExtraCreditMethods {
+public class ExtraCreditMethods {
+    public static int min(int values[]){
+        int lowestNumber = values[0];
+
+        for (int x = 1; x < values.length; x++) {
+            if (values[x] < lowestNumber) {
+                lowestNumber = values[x];
+            }
+        }
+
+        return  lowestNumber;
+    }
+    public static int max(int values[]){
+        int highestNumber = values[99];
+
+        for (int x = 1; x < values.length; x++) {
+            if (values[x] > highestNumber) {
+                highestNumber = values[x];
+            }
+        }
+
+        return  highestNumber;
+    }
+
+    public static int occuranceScan(int values[], int target){
+        int timesFound = 0;
+        for (int x = 0; x < values.length; x++) {
+            if (values[x] == target) {
+                timesFound +=1;
+            }
+        }
+        System.out.println("The integer " + target + " was found " + timesFound + " times");
+        return timesFound;
+    }
+
+    public static int sum(int values[]){
+        int sum = 0;
+        for (int x = 0; x < values.length; x++){
+            sum += values[x];
+        }
+        return sum;
+    }
+    public boolean contains(int values[], int target){
+        boolean Found = false;
+        for (int x = 0; x < values.length; x++) {
+            if (values[x] == target) {
+                Found = true;
+                break;
+            }
+            else {
+                Found = false;
+            }
+        }
+        return Found;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,84 @@
+import java.util.Random; //importing Random
+import java.util.Scanner; //importing Scanner
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // First Part
+        Scanner in = new Scanner(System.in);
+        Random rnd = new Random();
+        int sum = 0;
+        double avg = 0;
+        int [] datapoints;
+        datapoints = new int[100];
+        for (int x = 0; x < datapoints.length; x++){
+            datapoints[x] = rnd.nextInt(99) + 1;
+            sum += datapoints[x];
+            avg = sum / datapoints.length;
         }
+        for (int x = 0; x < datapoints.length; x++) {
+            System.out.print(datapoints[x] + "|");
+        }
+
+        System.out.printf("\nThe sum of the numbers in the table is %d, and the average of all of the numbers is %.3f", sum, avg);
+
+        //Second part
+        int rangedInt = SafeInput.getRangedInt(in, "\nEnter an integer ", 1, 100);
+        int timesFound = 0;
+        int location = 0;
+        for (int x = 0; x < datapoints.length; x++) {
+            if (datapoints[x] == rangedInt) {
+                timesFound +=1;
+            }
+        }
+        System.out.println("The integer " + rangedInt + " was found " + timesFound + " times");
+
+        rangedInt = SafeInput.getRangedInt(in, "Enter another integer ", 1, 100);
+        for (int x = 0; x < datapoints.length; x++) {
+            if (datapoints[x] == rangedInt) {
+                location = x;
+                break;
+            }
+        }
+        if (location == 0) {
+            System.out.println("The integer " + rangedInt + " was not found in the table.");
+        }
+        else {
+            System.out.println("The integer " + rangedInt + " was found at array index " + location + ".");
+        }
+
+        int highestNumber = datapoints[99];
+
+        for (int x = 1; x < datapoints.length; x++) {
+            if (datapoints[x] > highestNumber) {
+                highestNumber = datapoints[x];
+            }
+        }
+        System.out.println("The highest number in the data table is " + highestNumber);
+
+        int lowestNumber = datapoints[0];
+
+        for (int x = 1; x < datapoints.length; x++) {
+            if (datapoints[x] < lowestNumber) {
+                lowestNumber = datapoints[x];
+            }
+        }
+        System.out.println("The lowest number in the data table is " + lowestNumber);
+
+
+        System.out.println("The average of all of the numbers in the data table is " + getAverage(datapoints));
     }
+    public static double getAverage(int values[])
+    {
+        int sum = 0;
+        int avg = 0;
+        for (int x = 0; x < values.length; x++){
+            sum += values[x];
+            avg = sum/values.length;
+        }
+        return avg;
+    }
+
 }
+
