767359e612112d859ab899d4b6f268bda7593f90
kregoric <75627074+isicisawsum@users.noreply.github.com>
2024-03-21 21:12:25 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,76 +8,77 @@ public class Main {
         // First Part
         Scanner in = new Scanner(System.in);
         Random rnd = new Random();
+        
         int sum = 0;
         double avg = 0;
-        int [] datapoints;
-        datapoints = new int[100];
-        for (int x = 0; x < datapoints.length; x++){
+        int [] datapoints;//defining variables
+        datapoints = new int[100]; //making datapoints from 0 to 100
+        for (int x = 0; x < datapoints.length; x++){ //loop to add 100 numbers to array datapoints
             datapoints[x] = rnd.nextInt(99) + 1;
-            sum += datapoints[x];
-            avg = sum / datapoints.length;
+            sum += datapoints[x]; 
+            avg = sum / datapoints.length;//variables for later
         }
         for (int x = 0; x < datapoints.length; x++) {
-            System.out.print(datapoints[x] + "|");
+            System.out.print(datapoints[x] + "|"); //printing integers in the array and putting a | in between them
         }
 
-        System.out.printf("\nThe sum of the numbers in the table is %d, and the average of all of the numbers is %.3f", sum, avg);
+        System.out.printf("\nThe sum of the numbers in the table is %d, and the average of all of the numbers is %.3f", sum, avg); //outputting what the sum and average are from the previous variables
 
         //Second part
-        int rangedInt = SafeInput.getRangedInt(in, "\nEnter an integer ", 1, 100);
+        int rangedInt = SafeInput.getRangedInt(in, "\nEnter an integer ", 1, 100); //using methods to get a ranged integer
         int timesFound = 0;
-        int location = 0;
+        int location = 0; //defining more variables
+        
         for (int x = 0; x < datapoints.length; x++) {
             if (datapoints[x] == rangedInt) {
-                timesFound +=1;
+                timesFound +=1; //adding the amount of times the entered integer was found in the array
             }
         }
-        System.out.println("The integer " + rangedInt + " was found " + timesFound + " times");
+        System.out.println("The integer " + rangedInt + " was found " + timesFound + " times"); //outputting how many times it was found
 
-        rangedInt = SafeInput.getRangedInt(in, "Enter another integer ", 1, 100);
-        for (int x = 0; x < datapoints.length; x++) {
+        rangedInt = SafeInput.getRangedInt(in, "Enter another integer ", 1, 100); //using the same ranged int method
+        for (int x = 0; x < datapoints.length; x++) {//loop to find location of the integer
+            location +=1; //adding the location to be + 1 more
             if (datapoints[x] == rangedInt) {
-                location = x;
-                break;
+                break; //stop the loop
             }
         }
         if (location == 0) {
-            System.out.println("The integer " + rangedInt + " was not found in the table.");
+            System.out.println("The integer " + rangedInt + " was not found in the array."); //int is not found in the array
         }
         else {
-            System.out.println("The integer " + rangedInt + " was found at array index " + location + ".");
+            System.out.println("The integer " + rangedInt + " was found at array index " + location + "."); //int was found
         }
 
-        int highestNumber = datapoints[99];
+        int highestNumber = datapoints[99];//finding the greatest value of the array
 
-        for (int x = 1; x < datapoints.length; x++) {
+        for (int x = 1; x < datapoints.length; x++) { //loop to calculate the highest number
             if (datapoints[x] > highestNumber) {
                 highestNumber = datapoints[x];
             }
         }
-        System.out.println("The highest number in the data table is " + highestNumber);
+        System.out.println("The highest number in the data table is " + highestNumber); //outputting the highest number in the array
 
-        int lowestNumber = datapoints[0];
+        int lowestNumber = datapoints[0]; //finding the least value of the array
 
         for (int x = 1; x < datapoints.length; x++) {
             if (datapoints[x] < lowestNumber) {
                 lowestNumber = datapoints[x];
             }
         }
-        System.out.println("The lowest number in the data table is " + lowestNumber);
+        System.out.println("The lowest number in the data table is " + lowestNumber); //outputting the lowest number
 
-
-        System.out.println("The average of all of the numbers in the data table is " + getAverage(datapoints));
+        System.out.println("The average of all of the numbers in the data table is " + getAverage(datapoints)); //outputting average with method
     }
     public static double getAverage(int values[])
     {
-        int sum = 0;
-        int avg = 0;
-        for (int x = 0; x < values.length; x++){
-            sum += values[x];
-            avg = sum/values.length;
+        int sum = 0; 
+        int avg = 0; //defining variables
+        for (int x = 0; x < values.length; x++){ //loop to calculate avg and sum
+            sum += values[x]; //sum = all of the values
+            avg = sum/values.length; //avg = sum divided by length of the array.
         }
-        return avg;
+        return avg; //returning the average
     }
 
 }
